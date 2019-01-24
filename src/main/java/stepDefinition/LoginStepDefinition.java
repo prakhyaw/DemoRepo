4507584e085c66f47961774527f170807a63b046
Prakhya <prakhya92@gmail.com>
2019-01-24 17:50:40 +0530

    push

--- src/main/java/stepDefinition/LoginStepDefinition.java
+++ src/main/java/stepDefinition/LoginStepDefinition.java
@@ -1,9 +1,15 @@
 package stepDefinition;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -19,6 +25,7 @@ public class LoginStepDefinition {
 		
 		System.setProperty("webdriver.chrome.driver", "D:/Softwares/chromedriver_win32/chromedriver.exe");
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.freecrm.com");		
 	}
 	
@@ -63,11 +70,72 @@ public class LoginStepDefinition {
 	}
 
 	@Then("^user fills the form and saves$")
-	public void user_fills_the_form_and_saves(){
+	public void user_fills_the_form_and_saves() throws InterruptedException{
+		Select title = new Select(driver.findElement(By.name("title")));
+		title.selectByVisibleText("Mr.");
+		
+		String firstname = "first name";
+		driver.findElement(By.id("first_name")).sendKeys(firstname);
+		String lastname = "surname";
+		driver.findElement(By.id("surname")).sendKeys(lastname);
+		String name = firstname+lastname;
+		
+		Select suffix = new Select(driver.findElement(By.name("suffix")));
+		suffix.selectByVisibleText("II");		
+		
+		driver.findElement(By.xpath("//input[@name='contact_lookup_ref']//following-sibling::input[@value='Lookup']")).click();
+		
+		Set<String> windows = driver.getWindowHandles();
+		
+		Iterator<String> it = windows.iterator();
+		String parentwindow = it.next();
+		
+		while(it.hasNext()) {
+			String childwindow = it.next();
+			driver.switchTo().window(childwindow);
+			driver.manage().window().maximize();
+			driver.findElement(By.id("search")).sendKeys("test");
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			
+			driver.findElement(By.xpath("//a[contains(text(),'test1 test1')]")).click();
+		}
+		
+		
+		Thread.sleep(3000);
+		driver.switchTo().window(parentwindow);
+		System.out.println(driver.getCurrentUrl());
+		driver.findElement(By.xpath("//input[@name='receive_email' and @value='N']")).click();
+		driver.findElement(By.id("fieldId_birthday")).sendKeys("02-Jan-1992");
+		
+		driver.findElement(By.xpath("//input[@value='Load From Company']//following-sibling::input[@type='submit' and @value='Save']")).submit();
 	}
 
+	
+	@Then("^user verifies the contact$")
+	public void user_verifies_the_contact() {
+		user_is_on_Contacts_page();
+		List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"vContactsForm\"]/table/tbody/tr/td[2]/a"));
+		Iterator<WebElement> itr = rows.iterator();
+		int rowcount = 0;
+		while(itr.hasNext()) {
+			rowcount++;
+		}
+		System.out.println(rowcount);
+		
+		
+		
+		
+		//*[@id="vContactsForm"]/table/tbody/tr[3]/td[2]/strong
+		//*[@id="vContactsForm"]/table/tbody/tr[4]/td[2]/a
+		
+		//*[@id="vContactsForm"]/table/tbody/tr[11]/td[2]/a
+		
+	}
+	
+	
 	@Then("^close the browser$")
 	public void close_the_browser(){
+		driver.close();
 	}
 
 }
